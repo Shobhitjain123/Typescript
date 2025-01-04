793cb76e84e39abad41bba55a16af6a1994f0f9d
Shobhit Jain <42944341+Shobhitjain123@users.noreply.github.com>
2025-01-04 19:45:52 +0530

    Typescript Unions

---
+++ union.js
@@ -0,0 +1,22 @@
+var num = 22;
+num = 44;
+num = "asd";
+var shobhit = {
+    name: "shobhit", id: 123
+};
+shobhit = { username: "shobhit", id: 234 };
+//  function getdbID(id : number | string) {
+//     console.log(`DB ID is ${id}`);
+//  }
+getdbID(3);
+getdbID("3");
+function getdbID(id) {
+    if (typeof id === 'string') {
+        id.toUpperCase();
+    }
+}
+//array
+var data = [1, 2, 3];
+var data1 = ["thor", "spidey", "ironman"];
+var data2 = ["hero", "villan", 1, 2, 3]; // Allows mix of numbers and strings in same array
+var data3 = ["hero", "villan"]; // Allows all values either string only or all values number only
---
+++ union.ts
@@ -0,0 +1,40 @@
+let num: number | string = 22
+
+num = 44
+num = "asd"
+
+type User = {
+    name: string,
+    id: number
+}
+
+type Admin = {
+    username: string,
+    id: number
+}
+
+let shobhit: User | Admin = {
+    name: "shobhit", id: 123
+}
+shobhit = { username: "shobhit", id: 234 }
+
+
+//  function getdbID(id : number | string) {
+//     console.log(`DB ID is ${id}`);
+//  }
+
+getdbID(3)
+getdbID("3")
+
+function getdbID(id: number | string) {
+    if (typeof id === 'string') {
+        id.toUpperCase()
+    }
+}
+
+//array
+const data: number[] = [1, 2, 3]
+const data1: string[] = ["thor", "spidey", "ironman"]
+
+const data2: (string | number)[] = ["hero", "villan", 1, 2, 3] // Allows mix of numbers and strings in same array
+const data3: string[] | number[] = ["hero", "villan"] // Allows all values either string only or all values number only
